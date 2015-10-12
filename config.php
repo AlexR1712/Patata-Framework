664d8e626f1a8e0249de4136cad45acf2357c6e3
Ronald331 <Ronald.dev1@gmail.com>
2015-10-12 08:18:51 -0500

    Fixing Render

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
-define('IS_PRODUCTION', true); // Borrar luego
+// Variable utilizada por las librerias, la cual indica si se debe imprimir o no los errores en pantalla
+define('IS_PRODUCTION', true);
 // Clase por defecto, esto significa que se cargara "PageController" como controlador inicial
 define('CLASS_DEFAULT', 'Page');
 // Este atributo representa que metodo debe ejecutar en caso de que no se especifique un controlador via URL
 define('METHOD_DEFAULT', 'index');
 
-define('TITLE', 'PF'); // Borrar Luego
-define('DESCRIPTION', 'Hecho con PF :)'); // Borrar Luego
+// Variables de meta informacion (Usadas en la Libreria Render)
+define('TITLE', 'PF');
+define('DESCRIPTION', 'Hecho con PF :)');
